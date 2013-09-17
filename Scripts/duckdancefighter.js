b1ff0420021a818c7307428ff026d59f51235568
Ryan Esteves <snargledorf@gmail.com>
2013-09-17 17:04:21 -0400

    Added game loop Ducky now moves more smoothly

--- Scripts/duckdancefighter.js
+++ Scripts/duckdancefighter.js
@@ -1,35 +1,66 @@
 ﻿/// <reference path="jquery-2.0.3.js" />
-
-$('body').keydown(function (event) {
-
-    var min = $('#game_area').offset().left;
-    var max = min + $('#game_area').width();
-
-    switch (event.keyCode) {
-        case 37: // left
-            moveObjectLeft($('#ducky'), min, max);
-            break;       
-        case 39: // right
-            moveObjectRight($('#ducky'), min, max);
-            break;       
-    }
+$(document).ready(function (e) {
+    gameLoop();
 });
+
+
+function moveDuckyLeft(min, max) {
+    moveObjectLeft($('#ducky'));
+}
+
+function moveDuckyRight(min, max) {
+    moveObjectRight($('#ducky'));
+}
 
 var move_amt = 10;
 
-function moveObjectLeft(object, min, max) {
+function moveObjectLeft(object) {
+    var bounds = getGameAreaBounds();
+    var min = bounds.left;
+
     var objectLeft = object.offset().left;
 
     var new_left = ((objectLeft - move_amt < min) ? min : objectLeft - move_amt);
     object.offset({ left: new_left });
 }
 
-function moveObjectRight(object, min, max) {
-    var objectLeft = object.offset().left;
+function moveObjectRight(object) {
+    var bounds = getGameAreaBounds();
+    var max = bounds.right - object.width();
 
-    // Adjust max to compensate for objects width
-    max = max - object.width();
+    var objectLeft = object.offset().left;
 
     var new_left = ((objectLeft + move_amt > max) ? max : objectLeft + move_amt);
     object.offset({ left: new_left });
+}
+
+function getGameAreaBounds() {
+    var left = game_area.offset().left;
+    var right = left+game_area.width();
+    var top = game_area.offset().top;
+    var bottom = top+game_area.height();
+
+    return ({ left: left, right: right, top: top, bottom: bottom});
+}
+
+$('body').keydown(function (event) {
+    keys[event.keyCode] = true;
+}).keyup(function (event) {
+    delete keys[event.keyCode];
+});
+
+var game_area = $('#game_area');
+var keys = {};
+
+function gameLoop() {
+    
+    if (keys[37]) { // left
+        moveDuckyLeft();
+    }
+
+    if (keys[39]) { // right
+        moveDuckyRight();
+    }
+    
+    setTimeout(gameLoop, 20);
 }
