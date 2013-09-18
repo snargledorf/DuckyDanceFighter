8bec3424b213561c6c7f05ff4f0d48e64f14f8d8
Ryan Esteves <snargledorf@gmail.com>
2013-09-18 13:58:04 -0400

    Ducky drawn, added ducky bullets and shooting

--- Scripts/duckdancefighter.js
+++ Scripts/duckdancefighter.js
@@ -1,42 +1,11 @@
 ﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="http://code.createjs.com/easeljs-0.6.1.min.js" />
 
 var FPS = 60;
-
-var game_canvas_element = $('#game_canvas');
-var game_canvas = game_canvas_element.get(0).getContext("2d");
-var ducky = $('#ducky');
-
-$(document).ready(function (e) {
-    init();
-});
-
-function init() {
-    centerDucky();
-    startGameLoop();
-}
-
-var ducky = {
-    color: "#F00",
-    x: 0,
-    height: 32,
-    width: 32,
-    draw: function () {
-        game_canvas.fillStyle = this.color;
-        game_canvas.fillRect(this.x, game_canvas_element.height() - this.height, this.width, this.height);
-    }
-};
-
-function centerDucky() {
-    var duckyNewLeft = (game_canvas_element.width() / 2) - (ducky.width / 2);
-    ducky.x = duckyNewLeft;
-}
-
-function startGameLoop() {
-    setInterval(function () {
-        update();
-        draw();
-    }, 1000 / FPS);
-}
+var canvas;
+var stage;
+var ducky;
+var ducky_bullets = [];
 
 var keys = {};
 
@@ -46,43 +15,132 @@ $('body').keydown(function (event) {
     delete keys[event.keyCode];
 });
 
-function update() {
+$(document).ready(function (e) {
+    init();
+});
 
+function init() {
+    // Grab the canvas and create the stage
+    canvas = document.getElementById('game_canvas');
+    stage = new createjs.Stage(canvas);
+    drawDucky();
+    startGameLoop();
+}
+
+function drawDucky() {
+
+    var spriteSheetData = {
+        animations: {
+            dance:  [0, 26, 'dance', 4]
+        },
+        images: ['Content/images/ducky_sprite_map.bmp'],
+        frames: {
+            regX: 0,
+            height: 100,
+            width: 100,
+            count: 27
+        }
+    };
+
+    var spriteSheet = new createjs.SpriteSheet(spriteSheetData);
+
+    ducky = new createjs.BitmapAnimation(spriteSheet);
+
+    ducky.y = canvas.height - spriteSheetData.frames.height;
+    ducky.x = (canvas.width - 100) / 2;
+
+    ducky.gotoAndPlay('dance');
+
+    stage.addChild(ducky);
+}
+
+function startGameLoop() {
+    createjs.Ticker.setFPS(FPS);
+    createjs.Ticker.addListener(gameLoop);
+}
+
+function gameLoop(e) {    
+    update();
+    stage.update();
+}
+
+function update() {
     if (keys[37]) { // left
         moveDuckyLeft();
     }
-
     if (keys[39]) { // right
         moveDuckyRight();
     }
-
     if (keys[32]) { // space
         shootDucky();
     }
+
+    updateBullets();
 }
 
-function draw() {
-    drawGameArea();
-    ducky.draw();
-}
-
-function drawGameArea() {
-    game_canvas.clearRect(0,0,game_canvas_element.width(), game_canvas_element.height());
-    game_canvas.fillStyle = "#FFF";
-    game_canvas.fillRect(0, 0, game_canvas_element.width(), game_canvas_element.height());
-}
-
-var move_speed = 5;
+var move_speed = 10;
 function moveDuckyLeft() {
-    var new_x = ducky.x - move_speed < 0 ? 0 : ducky.x - move_speed;
-    ducky.x = new_x;
+    var new_left = (ducky.x - move_speed) < 0 ? 0 : ducky.x - move_speed;
+    ducky.x = new_left;
 }
 
 function moveDuckyRight() {
-    var new_x = (ducky.x + move_speed) > (game_canvas_element.width() - ducky.width) ? (game_canvas_element.width() - ducky.width) : (ducky.x + move_speed);
-    ducky.x = new_x;
+    var new_left = (ducky.x + 100 + move_speed) > canvas.width ? canvas.width - 100 : ducky.x + move_speed;
+    ducky.x = new_left;
 }
-
+var lastBulletShotCount = 0;
 function shootDucky() {
 
+    if (lastBulletShotCount > 0) {
+        lastBulletShotCount--;
+        return;
+    }
+
+    lastBulletShotCount = 10;
+
+    var spriteSheetData = {
+        animations: {
+            dance: [0, 26, 'dance', 4]
+        },
+        images: ['Content/images/ducky_sprite_map.bmp'],
+        frames: {
+            regX: 0,
+            height: 100,
+            width: 100,
+            count: 27
+        }
+    };
+
+    var spriteSheet = new createjs.SpriteSheet(spriteSheetData);
+
+    var bullet = new createjs.BitmapAnimation(spriteSheet);
+
+    bullet.y = ducky.y;
+    bullet.x = ducky.x + (75 / 2);
+    bullet.scaleX = .25;
+    bullet.scaleY = .25;
+
+    bullet.gotoAndPlay('dance');
+
+    stage.addChild(bullet);
+
+    ducky_bullets.push(bullet);
+}
+
+function updateBullets() {
+    ducky_bullets.forEach(function (e) {
+        e.y -= 10;
+        if (e.y < -25) {
+            removeBullet(e);
+        }
+    });
+}
+
+function removeBullet(bullet) {
+    var index = ducky_bullets.indexOf(bullet);
+    if (index >= 0) {
+        ducky_bullets.splice(index, 1);
+    }
+
+    stage.removeChild(bullet);
 }
